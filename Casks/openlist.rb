9fc2534abaa8359c283bf1a7da94836c89d083a8
ivaquero <msaintms@outlook.com>
2025-07-30 16:09:24 +0800

    throne: update to 1.0.0-beta.6

--- Casks/openlist.rb
+++ Casks/openlist.rb
@@ -15,7 +15,7 @@ cask "openlist" do
   homepage "https://oplist.org/"
 
   livecheck do
-    url "https://github.com/OpenListTeam/OpenList"
+    url :url
     strategy :github_latest
   end
 
@@ -31,7 +31,7 @@ cask "openlist" do
   caveats <<~EOS
     To reveal openlist admin user's info in default `config.json` again, run the following command:
       cd #{staged_path} && openlist admin
-    Or reveal `admin` password via `sqlite3` command (before v3.25.1):
+    Or reveal `admin` password via `sqlite3` command:
       sqlite3 etc/openlist/data.db "select password from x_users where username = 'admin'"
     Or reset `admin` password:
       cd #{staged_path} && openlist admin random
