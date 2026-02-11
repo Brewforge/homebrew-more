7851c411f3b995ed8513083f6ba9ded772f99c9b
ivaquero <msaintms@outlook.com>
2026-02-11 23:08:02 +0800

    rheo: fix url

--- Casks/rheo.rb
+++ Casks/rheo.rb
@@ -8,7 +8,7 @@ cask "rheo" do
          arm64_linux:  "ae52aed9192ab9dd18b3f578d9654efac4676f045b8e0fcf7baf20fc4d35ff04",
          x86_64_linux: "ae52aed9192ab9dd18b3f578d9654efac4676f045b8e0fcf7baf20fc4d35ff04"
 
-  url "https://github.com/freecomputinglab/rheo/releases/download/v#{version}/rheo-#{arch}-#{os}.tar.gz",
+  url "https://github.com/freecomputinglab/rheo/releases/download/v#{version}/rheo-#{arch}-#{os}.zip",
       verified: "github.com/freecomputinglab/rheo/"
   name "rheo"
   desc "Typesetting and static site engine based on Typst"
