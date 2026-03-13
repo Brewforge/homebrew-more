e1cb5488c5b2cd23095e0a32bb77c3d35fffe7d6
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-03-14 00:35:29 +0800

    xytz 0.8.7 (#70)

--- Casks/xytz.rb
+++ Casks/xytz.rb
@@ -2,11 +2,11 @@ cask "xytz" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "0.8.6"
-  sha256 arm:          "4c40a447ed89767297c5c871fcf34f3b3d82fd238286de48a55de2c269e02faf",
-         x86_64:       "1eb8e4283495c1b3819d0d5bc7b3b19e39f068a98ab9e1104782b39168fe93c9",
-         arm64_linux:  "f425ccceba09c36862a031f712418bdccdd74e73c922dd60337148b0db9723cc",
-         x86_64_linux: "8a2013545ab7c8fe324d4a3235d1f5cd463f1901c00e9b6aaf70e950a9eea88c"
+  version "0.8.7"
+  sha256 arm:          "f2896d492b754e76f3f76299bbbd14f89d7a5e2fae6933862534e9883468f848",
+         x86_64:       "b06eee52c32768d8991fd2a219ad9a0b868c22394fbed66659be8a0888b258d2",
+         arm64_linux:  "916acf739276d01533eb451eb389f2f4d2950ef0c8e2395eaadf03ac44951d50",
+         x86_64_linux: "f56bd87c44b886b05f37c97998af4b21ca48b3a522e1841789d8c7d13f493d00"
 
   url "https://github.com/xdagiz/xytz/releases/download/v#{version}/xytz-v#{version}-#{os}-#{arch}.tar.gz"
   name "xytz"
