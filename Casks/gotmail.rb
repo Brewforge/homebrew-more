9e1f5463f17c338cba8592ed7b709cbe3c9ca66d
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-04-15 12:16:18 +0800

    gotmail 0.3.1 (#76)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/gotmail.rb
+++ Casks/gotmail.rb
@@ -2,11 +2,11 @@ cask "gotmail" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "0.3.0"
-  sha256 arm:          "95ce346b7071ece1c9d1d444727f6eda590f13b338089d8d2676b7171f767bf9",
-         x86_64:       "24910af11f73f9d666635fe4974fe5a14b1ca726b30e6ceba102f7f54d90d664",
-         arm64_linux:  "60a157c53d7dd03af8384727ef246e181164f3f726784554284e9786a492b159",
-         x86_64_linux: "1423bee9ea407268528c440f661484ef9dbbf0d5e498236afaa8f87e350f2acc"
+  version "0.3.1"
+  sha256 arm:          "d182c6fd408ede1301a56d3d925886d5200a463cdff6f52c5b7c1f1f7a969ff9",
+         x86_64:       "f316799d6807f94c65fcb9b5599f5303a0273b976a143dedb7e9f405451453b8",
+         arm64_linux:  "b5f62b5c0596357ccdf04e9c9f25c42b4128c1375c09bfeb327f7cbe8472fb6c",
+         x86_64_linux: "e10da58949cffca4d4a53e4f2b80c4c0e9c17750902dd3a47572c6d18cf8879f"
 
   url "https://github.com/ivaquero/gotmail/releases/download/v#{version}/gotmail_#{version}_#{os}_#{arch}.tar.gz"
   name "gotmail"
