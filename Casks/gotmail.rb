1653063e94482d12a62ab16dd73d441d80341bfb
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-04-15 19:23:49 +0800

    gotmail 0.3.2 (#77)

--- Casks/gotmail.rb
+++ Casks/gotmail.rb
@@ -2,11 +2,11 @@ cask "gotmail" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "0.3.1"
-  sha256 arm:          "d182c6fd408ede1301a56d3d925886d5200a463cdff6f52c5b7c1f1f7a969ff9",
-         x86_64:       "f316799d6807f94c65fcb9b5599f5303a0273b976a143dedb7e9f405451453b8",
-         arm64_linux:  "b5f62b5c0596357ccdf04e9c9f25c42b4128c1375c09bfeb327f7cbe8472fb6c",
-         x86_64_linux: "e10da58949cffca4d4a53e4f2b80c4c0e9c17750902dd3a47572c6d18cf8879f"
+  version "0.3.2"
+  sha256 arm:          "34ef8487c57c2a251acba5a844af1c5348664d35bca0a856ba32ac1e0ca6e69b",
+         x86_64:       "3e6162790f396c601f1028465c088e1e6cdfe0ea259727bcdbedbc9620b57464",
+         arm64_linux:  "eaa308a3e35141e48df08b260d02b4a447e54a53e8a5d5d87b95708eec50fd4b",
+         x86_64_linux: "655e7a78476f58040282cef45bda5f7bdf6469140241a4e5d0c1abb9ea45b9c4"
 
   url "https://github.com/ivaquero/gotmail/releases/download/v#{version}/gotmail_#{version}_#{os}_#{arch}.tar.gz"
   name "gotmail"
