46ffef4fcd807b3db989fb2e2a8a55c59e455d12
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-04-16 21:41:13 +0800

    rheo 0.2.1 (#78)

--- Casks/rheo.rb
+++ Casks/rheo.rb
@@ -2,11 +2,11 @@ cask "rheo" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "apple-darwin", linux: "unknown-linux-gnu"
 
-  version "0.2.0"
-  sha256 arm:          "1a3eba53960a3879aad6b383cec6da21b767b9b5f3ccf50779be6a9581af69ff",
-         x86_64:       "1a3eba53960a3879aad6b383cec6da21b767b9b5f3ccf50779be6a9581af69ff",
-         arm64_linux:  "bc6568572b8568439e4e92175302a8b629cf16c6bb8b10de8785f3ecc5a5d75d",
-         x86_64_linux: "bc6568572b8568439e4e92175302a8b629cf16c6bb8b10de8785f3ecc5a5d75d"
+  version "0.2.1"
+  sha256 arm:          "0d3c9bf27241deb6a91f736bdf6426bcf02a11c38261b15cfaf943121ba79fe3",
+         x86_64:       "0d3c9bf27241deb6a91f736bdf6426bcf02a11c38261b15cfaf943121ba79fe3",
+         arm64_linux:  "6b094439c86cf7b3848565e5163531e5705ea574511967bffbe50253e5582ad7",
+         x86_64_linux: "6b094439c86cf7b3848565e5163531e5705ea574511967bffbe50253e5582ad7"
 
   url "https://github.com/freecomputinglab/rheo/releases/download/v#{version}/rheo-#{arch}-#{os}.zip",
       verified: "github.com/freecomputinglab/rheo/"
